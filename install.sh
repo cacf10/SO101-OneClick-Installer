4e088d46fb9f627da5c88ff6050d0ab92f28ee9a
cacf10 <cacf10@163.com>
2026-08-08 16:16:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ set -e
 
 ROOT_DIR="$(cd "$(dirname "$0")"; pwd)"
 
-source "$ROOT_DIR/lib/colors.sh"
-source "$ROOT_DIR/lib/logger.sh"
-source "$ROOT_DIR/lib/check.sh"
-source "$ROOT_DIR/lib/utils.sh"
+source "$ROOT_DIR/libs/colors.sh"
+source "$ROOT_DIR/libs/logger.sh"
+source "$ROOT_DIR/libs/check.sh"
+source "$ROOT_DIR/libs/utils.sh"
 
 echo_blue "====================================="
 echo_blue " SO101 One Click Installer V1.0"
@@ -15,15 +15,15 @@ echo_blue "====================================="
 
 check_system
 
-bash scripts/install_conda.sh
+bash scripts/03_conda.sh
 
-bash scripts/create_env.sh
+bash scripts/04_environment.sh
 
-bash scripts/install_torch.sh
+bash scripts/06_torch.sh
 
-bash scripts/install_lerobot.sh
+bash scripts/07_lerobot.sh
 
-bash scripts/setup_usb.sh
+bash scripts/08_usb.sh
 
 echo_green ""
 echo_green "Installation Finished!"
