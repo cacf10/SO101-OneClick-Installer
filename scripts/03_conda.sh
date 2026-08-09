302fa91879d7f316aea88f7ecd30a44752efc5b1
cacf10 <cacf10@163.com>
2026-08-09 17:36:38 +0800

    Update 03_conda.sh

--- scripts/03_conda.sh
+++ scripts/03_conda.sh
@@ -183,3 +183,19 @@ echo "Python       : $(python --version)"
 echo
 
 success "STEP 3 Finished."
+
+#!/usr/bin/env bash
+
+MINIFORGE_DIR="$HOME/miniforge3"
+
+
+load_conda(){
+
+    if [ -f "$MINIFORGE_DIR/etc/profile.d/conda.sh" ]; then
+        source "$MINIFORGE_DIR/etc/profile.d/conda.sh"
+    else
+        echo "Conda not found"
+        exit 1
+    fi
+
+}
